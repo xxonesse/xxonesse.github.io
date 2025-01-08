168eaef14cb3245615a20dd9c869838a71d642c3
_CoDumbx01 <jarawandasalinas@gmail.com>
2025-01-08 09:02:17 +0800

    modified

--- js/main.js
+++ js/main.js
@@ -4,18 +4,17 @@ import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/l
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 25;
 
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
 let object;
-let controls;
-let objToRender = 'scifiCube_test';
 
 const loader = new GLTFLoader();
 
-// Load the file
+// Load the GLB file
 loader.load(
-    `models/${objToRender}/scene.gltf`,
+    'models/scifiCube_test.glb',
     function (gltf) {
         object = gltf.scene;
         scene.add(object);
@@ -24,33 +23,28 @@ loader.load(
         console.log((xhr.loaded / xhr.total * 100) + '% loaded');
     },
     function (error) {
-        console.error(error);
+        console.error('An error happened', error);
     }
 );
 
 const renderer = new THREE.WebGLRenderer({ alpha: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-
 document.getElementById("container3d").appendChild(renderer.domElement);
-
-camera.position.z = objToRender === "scifiCube_test" ? 25 : 500;
 
 const topLight = new THREE.DirectionalLight(0xffffff, 1);
 topLight.position.set(500, 500, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "scifiCube_test" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0x333333, 5);
 scene.add(ambientLight);
 
-if (objToRender === "scifiCube_test") {
-    controls = new OrbitControls(camera, renderer.domElement);
-}
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
     requestAnimationFrame(animate);
 
-    if (object && objToRender === "scifiCube_test") {
+    if (object) {
         object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
         object.rotation.x = -1.2 + (mouseY * 2.5 / window.innerHeight);
     }
