a8be27babfe35651b9476b3261a08706bace743c
_CoDumbx01 <jarawandasalinas@gmail.com>
2025-01-08 08:59:59 +0800

    fixed

--- js/main.js
+++ js/main.js
@@ -1,25 +1,20 @@
-import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.modules.js";
-
-import { OrbitControls } from "https://cdn.skypack.dev/three@0.0129.0/examples/jsm/controls/OrbitControls.js";
-
+import * as THREE from "https://cdn.skypack.dev/three@0.129.0/build/three.module.js";
+import { OrbitControls } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js";
 import { GLTFLoader } from "https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js";
 
 const scene = new THREE.Scene();
-const camera = new THREE.Perspectivecamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 let mouseX = window.innerWidth / 2;
 let mouseY = window.innerHeight / 2;
-
 let object;
-
 let controls;
-
 let objToRender = 'scifiCube_test';
 
 const loader = new GLTFLoader();
 
-//Load the file
-loader.load (
+// Load the file
+loader.load(
     `models/${objToRender}/scene.gltf`,
     function (gltf) {
         object = gltf.scene;
@@ -40,12 +35,12 @@ document.getElementById("container3d").appendChild(renderer.domElement);
 
 camera.position.z = objToRender === "scifiCube_test" ? 25 : 500;
 
-const topLight = new THREE.DirectionalLight(0xffffff, 1); 
-topLight.position.set(500, 500, 500),
+const topLight = new THREE.DirectionalLight(0xffffff, 1);
+topLight.position.set(500, 500, 500);
 topLight.castShadow = true;
 scene.add(topLight);
 
-const ambientLight = new THREE.Ambientlight(0x333333, objToRender === "scifiCube_test" ? 5 : 1);
+const ambientLight = new THREE.AmbientLight(0x333333, objToRender === "scifiCube_test" ? 5 : 1);
 scene.add(ambientLight);
 
 if (objToRender === "scifiCube_test") {
@@ -56,16 +51,16 @@ function animate() {
     requestAnimationFrame(animate);
 
     if (object && objToRender === "scifiCube_test") {
-        object.rotation.y = -3 + mouseX / window.innerWidth * 3;
-        object.rotation.x = -1.2 + mouseY * 2.5 / w.innerHeight;
+        object.rotation.y = -3 + (mouseX / window.innerWidth) * 3;
+        object.rotation.x = -1.2 + (mouseY * 2.5 / window.innerHeight);
     }
     renderer.render(scene, camera);
 }
 
-window.addEventListener("resize", function() {
-    camera.aspect = this.window.innerWidth / this.window.innerHeight;
-    camera.updateProjectMatrix();
-    renderer.setSize(window.innerWidth, this.window.innerHeight);
+window.addEventListener("resize", function () {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
 document.onmousemove = (e) => {
